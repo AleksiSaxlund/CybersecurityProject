e44015a79858081130f89acf89afb9c44917209e
Aleksi Saxlund <saxlunda@ad.helsinki.fi>
2026-05-05 19:48:21 +0300

    Implemented main functionality

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add_note, name='add_note'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Note
 
 def index(request):
-    return HttpResponse("Hello world.")
+    query = request.GET.get('query', '')
+
+    if query:
+        sql_query = f"SELECT * FROM core_note WHERE content LIKE '%{query}%'"
+        notes = Note.objects.raw(sql_query)
+
+        # Alhaalla korjaus tälle haavoittuvaisuudelle.
+        #
+        # notes = Note.objects.filter(content__icontains=query)
+    else:
+        notes = Note.objects.all()
+
+    return render(request, 'core/index.html', {'notes': notes})
+
+def add_note(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+
+        if request.user.is_authenticated and content:
+            Note.objects.create(user=request.user, content=content)
+    
+    return redirect('index')
+
+def delete_note(request, note_id):
+    note = Note.objects.get(id=note_id)
+    note.delete()
+    return redirect('index')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'core/auth.html', {'form': form, 'title': 'Register'})
+
+def user_login(request,):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'core/auth.html', {'form': form, 'title': 'Login'})
+
+def user_logout(request):
+    if request.method == 'POST':
+        logout(request)
+    
+    return redirect('index')
